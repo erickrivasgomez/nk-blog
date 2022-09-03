74221dd21541f94d3d67e69c2dab4ca719d227bd
Erick Rivas Gomez <rivaserick@outlook.com>
2022-09-02 23:29:16 -0500

    Refactor submitComment function

--- services/index.js
+++ services/index.js
@@ -207,15 +207,13 @@ export const getFeaturedPosts = async () => {
 };
 
 export const submitComment = async (obj) => {
-  const baseUrl = process.env.BASE_URL;
-  const result = { obj, baseUrl };
-  /* const result = await fetch(`${baseUrl}/api/comments`, {
+  const result = await fetch('/api/comments', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(obj),
-  }); */
+  });
 
   return result.json();
 };

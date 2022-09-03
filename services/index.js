1bfee5b19afb21fdc9ceaf0d396cc2c0354365b8
Erick Rivas Gomez <rivaserick@outlook.com>
2022-09-02 22:27:48 -0500

    Only fetch when global.window

--- services/index.js
+++ services/index.js
@@ -207,13 +207,16 @@ export const getFeaturedPosts = async () => {
 };
 
 export const submitComment = async (obj) => {
-  const result = await fetch('/api/comments', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(obj),
-  });
+  let result = {};
+  if (global.window) {
+    result = await fetch('/api/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    });
+  }
 
   return result.json();
 };
